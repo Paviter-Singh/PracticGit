c5567a29d8f7a08cc5836c121335f15ce9f15f78
Paviter Singh <pavitersingh@testingxperts.com>
2022-06-08 12:44:09 +0530

    changed file for test

---
+++ MyAppT/Infrastructure/TeslaStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAppT.Infrastructure
+{
+    public class TeslaStock
+    {
+        public int Predict(int currentValue)
+        {
+            int newValue = Convert.ToInt32(currentValue + (.5 * currentValue));
+            return newValue;
+        }
+    }
+}
+
+//testing 
